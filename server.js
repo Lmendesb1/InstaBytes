d00475bc4dd38db23187838e2a3ca6a33ec9e630
Lmendesb1 <lucasprata2010@gmail.com>
2024-11-20 21:32:18 +0000

    Conectando a API ao MongoDB

--- server.js
+++ server.js
@@ -1,42 +1,10 @@
 import express from "express";
+import routes from "./src/config/routes/postsRoutes.js";
 
-const posts = [
-    {
-        id: 1,
-      descricao: "Uma foto teste",
-      imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 2,
-      descricao: "Gato curioso olhando pela janela",
-      imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 3,
-      descricao: "Cachorro feliz brincando no parque",
-      imagem: "https://placecats.com/millie/300/150",
-    },
-  ];
 
 const app = express();
-app.use(express.json());
+routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando");
-});
-
-app.get("/posts", (req,res) => {
-    res.status(200).json(posts);
-});
-
-//Recebe um id, entra no array de posts e vê se o id bate 
-function buscarPostPorID(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts/:id", (req,res) => {
-    const index = buscarPostPorID(req.params.id) 
-    res.status(200).json(posts[index]);
 });
